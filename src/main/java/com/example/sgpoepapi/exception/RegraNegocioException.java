458752c0602969a7eeeaab2befabac9856fa69e1
gabhriel <gabhriel.velasco@estudante.ufjf.br>
2026-05-19 21:58:12 -0300

    criei exception e coloquei os comentarios

---
+++ src/main/java/com/example/sgpoepapi/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package com.example.sgpoepapi.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
